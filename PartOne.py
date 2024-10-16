7c1103ac3af5c8abdcd65577e885e1ee8a9331ce
eb582 <eb582@kent.ac.uk>
2024-10-16 11:20:09 +0000

    Over comlicated camel to snake

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,14 @@
+def change_case(name):
+    res=[name[0].lower()]
+    for c in name[1:]:
+        if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+            res.append('_')
+            res.append(c.lower())
+        else:
+            res.append(c)
+    
+    return ''.join(res)
 
+name = input("Whats your name? ")
+name = name.replace(" ","")
+print(change_case(name), end="")
